fb54ee2f42fffbdb3cadc7e5679cf12215ad6701
Lakshmanaperumal L M <laksmurugesan04@gmail.com>
2025-08-22 11:40:29 +0530

    Delete day2/compression.py

--- day2/compression.py
+++
@@ -1,6 +0,0 @@
-import cv2
-
-img = cv2.imread("build.jpg")
-
-cv2.imwrite("compressed_image_30.jpeg",img,[int(cv2.IMWRITE_JPEG_QUALITY),30])
-cv2.imwrite("compressed_image_90.jpeg",img,[int(cv2.IMWRITE_JPEG_QUALITY),90])
