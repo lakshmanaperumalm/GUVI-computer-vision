5fafa09b422f70a5cf1dbc900b9395a93d7d49d3
Lakshmanaperumal L M <laksmurugesan04@gmail.com>
2025-08-22 09:29:33 +0530

    Add files via upload

---
+++ day4/comp.py
@@ -0,0 +1,8 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+img=cv2.imread("lion.jpg")
+cv2.imwrite("compressed.jpeg",img,[cv2.IMWRITE_JPEG_QUALITY,50])
+cv2.imwrite("compressed.png",img,[cv2.IMWRITE_PNG_COMPRESSION,9])
+cv2.imwrite("compressed.webp",img,[cv2.IMWRITE_WEBP_QUALITY,50])
+
